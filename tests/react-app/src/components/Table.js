0a6083174ff58ec583171ca7b85f3c074502132a
MagicJesus <46258083+MagicJesus@users.noreply.github.com>
2021-02-12 16:07:08 +0100

    Table Changes

--- tests/react-app/src/components/Table.js
+++ tests/react-app/src/components/Table.js
@@ -2,11 +2,14 @@ import React from "react";
 import TableRow from "./TableRow";
 
 const Table = ({ name, data }) => {
+  var keys = [];
+  Object.keys(data[0]).forEach((key) => keys.push(key + " | "));
   return (
     <ul className="list">
+      <li className="list-title">{name}</li>
+      <li> {keys}</li>
       <li>
-        <h1>{name}</h1>
-        <ul className="list">
+        <ul>
           {data.map((row) => {
             return <TableRow key={row.id} contents={row} />;
           })}
