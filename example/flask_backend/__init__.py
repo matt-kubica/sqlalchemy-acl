76f509ad9fa3cc629f1bc6e367333dfd5a0a7d52
qbic22562 <mateusz.kubica99@gmail.com>
2021-02-14 23:36:19 +0100

    Refactoring

--- example/flask_backend/__init__.py
+++ example/flask_backend/__init__.py
@@ -1,47 +1,50 @@
 import sys, os
 sys.path.insert(0, '../..')
 
-from flask import Flask
+from flask import Flask as BaseFlask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
+
+# extends flask with method that allows registering api endpoint in more optimal way
+class Flask(BaseFlask):
+
+    def register_api(self, view, endpoint, url, pk='id', pk_type='int'):
+        view_func = view.as_view(endpoint)
+        self.add_url_rule(url, defaults={pk: None}, view_func=view_func, methods=['GET'])
+        self.add_url_rule(url, view_func=view_func, methods=['POST'])
+        self.add_url_rule('{0}<{1}:{2}>'.format(url, pk_type, pk), view_func=view_func,
+                          methods=['GET', 'PUT', 'DELETE'])
+
+
+# constants
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite') + '?check_same_thread=False'
 ACL_CONFIG_PATH = os.path.join(BASE_DIR, 'acl-config.yaml')
 
+# config
 db = SQLAlchemy()
 app = Flask(__name__)
 app.app_context().push()
 CORS(app, resources={r"/*": {"origins": "*"}})
-
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'my-ultra-secret-key'
 
 
-
-def register_api(app, view, endpoint, url, pk='id', pk_type='int'):
-    view_func = view.as_view(endpoint)
-    app.add_url_rule(url, defaults={pk: None}, view_func=view_func, methods=['GET'])
-    app.add_url_rule(url, view_func=view_func, methods=['POST'])
-    app.add_url_rule('{0}<{1}:{2}>'.format(url, pk_type, pk), view_func=view_func, methods=['GET', 'PUT', 'DELETE'])
-
-
+# helper methods
 def register_blueprints(app):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-
 def register_endpoints(app):
     from .api import SalariesAPI, CustomersAPI, OrdersAPI, ArticlesAPI, BoxesAPI, ContentsAPI
-    register_api(app, SalariesAPI, 'salaries_api', '/salaries/')
-    register_api(app, CustomersAPI, 'customers_api', '/customers/')
-    register_api(app, OrdersAPI, 'orders_api', '/orders/')
-    register_api(app, ArticlesAPI, 'articles_api', '/articles/')
-    register_api(app, BoxesAPI, 'boxes_api', '/boxes/')
-    register_api(app, ContentsAPI, 'contents_api', '/contents/')
-
-
+    app.register_api(SalariesAPI, 'salaries_api', '/salaries/')
+    app.register_api(CustomersAPI, 'customers_api', '/customers/')
+    app.register_api(OrdersAPI, 'orders_api', '/orders/')
+    app.register_api(ArticlesAPI, 'articles_api', '/articles/')
+    app.register_api(BoxesAPI, 'boxes_api', '/boxes/')
+    app.register_api(ContentsAPI, 'contents_api', '/contents/')
 
 def setup_acl(engine):
     from sqlalchemy_acl import ACL
@@ -49,8 +52,3 @@ def setup_acl(engine):
     ACL.setup(engine, user_model=UserModel, access_levels_config=ACL_CONFIG_PATH)
 
 
-db.init_app(app)
-register_blueprints(app)
-register_endpoints(app)
-db.create_all(app=app)
-setup_acl(engine=db.get_engine(app))
--- example/flask_backend/app.py
+++ example/flask_backend/app.py
@@ -1,7 +1,21 @@
 from . import app, db, register_endpoints, register_blueprints, setup_acl
+from sqlalchemy_utils import database_exists, create_database, drop_database
 
+db.init_app(app)
+register_blueprints(app)
+register_endpoints(app)
+engine = db.get_engine(app)
 
-if __name__ == '__main__':
+if not database_exists(engine.url):
+    print(' * Creating database...')
+    db.create_all(app=app)
+    create_database(engine.url)
+else:
+    print(' * Database already exist...')
 
+    # for now
+    drop_database(engine.url)
+    db.create_all(app=app)
+    create_database(engine.url)
 
-    app.run(debug=False)
+setup_acl(engine=db.get_engine(app))
