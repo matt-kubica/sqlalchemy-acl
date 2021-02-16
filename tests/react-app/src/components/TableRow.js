a61e30497891f52b6d2cba68881e5fa275e4ba36
qbic22562 <mateusz.kubica99@gmail.com>
2021-02-16 13:31:10 +0100

    Deleted old, unnecesary files

--- tests/react-app/src/components/TableRow.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TableRow = ({ contents }) => {
-  var fields = [];
-  Object.keys(contents).forEach((key) => fields.push(contents[key] + " | "));
-  return <li>{fields}</li>;
-};
-
-export default TableRow;
